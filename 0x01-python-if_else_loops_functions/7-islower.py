9b42e938c27088ee7f51dd60428080dcc9acf8ed
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-08 11:33:08 +0000

    added task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for ch in range(97, 123):
-        lower_case = chr(ch)
-        if lower_case == c:
+        lower_case = ord(c)
+        if ch == lower_case:
             return (True)
