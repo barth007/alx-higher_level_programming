1e2c6e93cd157dff561f23ef219df5511e5d3184
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-08 07:52:31 +0000

    added task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,5 +5,5 @@ while number <= 99:
         print("0{}, ".format(number), end="")
     elif number != 100 and number >= 10:
         print("{}, ".format(number)
-              if number != 99 else "{}".format(number), end="")
+              if number != 99 else "{} ".format(number), end="")
     number += 1
