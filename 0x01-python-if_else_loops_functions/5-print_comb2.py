ff2ad5bc658d43696e902275284da76b84882526
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-08 08:53:54 +0000

    adde the task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 number = 0
 while number <= 99:
-    if number < 10:
+    if number < 99:
         zero = 0
-        print("{:d}{:d}, ".format(zero, number), end="")
-    elif number != 100 and number >= 10:
-        print("{:d}, ".format(number)
-              if number != 99 else "{} ".format(number), end="")
+        print("{:d}{:d}, ".format(zero, number)
+              if number < 10 else "{}, ".format(number), end="")
+    else:
+        print("{:d} ".format(number))
     number += 1
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+number1 = 0
+while number1 < 10:
+    number2 = number1 + 1
+    while number2 < 10:
+        if number1 != 8:
+            print("{}{}, ".format(number1, number2), end="")
+        else:
+            print("{}{} ".format(number1, number2))
+        number2 += 1
+    number1 += 1
