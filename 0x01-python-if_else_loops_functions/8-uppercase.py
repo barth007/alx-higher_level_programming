f69d453202e76ffc98db481a1f61b4470bce16e4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-08 23:02:59 +0000

    added task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+uppercase - function prints a string in uppercase
+@str: argument to be convented to uppercase
+"""
+
+
+def uppercase(str):
+    msg = ' '
+    for strs in str:
+        if ord(strs) >= 97 and ord(strs) <= 122:
+            strs = chr(ord(strs) - 32)
+        msg = msg + strs
+    print("{}\n".format(msg))
