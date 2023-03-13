dcffb75de8698b7d3f358963891d5ebadbdd2b7f
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-13 06:03:31 +0000

    added task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     import add_0
     a = 1
     b = 2
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    result = add_0.add(a,b)
+    print("{} + {} = {}".format(a, b, result))
