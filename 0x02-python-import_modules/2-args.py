eb7726f835cf14ba9e5ce86c435920545b8f6a1e
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-13 12:59:42 +0000

    added task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     import sys
     arg_number = len(sys.argv) - 1
     if arg_number == 0:
-        print("{} arguments.".format(arg_number))
+        print("{} arguments.\n".format(arg_number))
     else:
-        print("{} argument{}".format(arg_number,
+        print("{} argument{}\n".format(arg_number,
               's'if arg_number != 1 else ' '))
         for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+            print("{}: {}\n".format(i, arg))
