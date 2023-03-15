864a08cf25e8da20e92958f3102eef99302e5f1f
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-15 13:02:11 +0000

    added task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
+    if not isinstance(sentence, str):
+        return (None, )
+    elif not sentence:
         return (None, )
     else:
         length = len(sentence)
