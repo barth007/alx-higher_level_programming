195696c0dbf4731d7406b483f5830e664e634bdd
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-16 12:02:20 +0000

    added task 0 and README.md file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""
 def square_matrix_simple(matrix=[]):
+    """
+    computes the square value of all integers of a matrix.
+    Args:
+    matrix (list[list[int]]): A 2-dimensional matrix of integers.
+    Returns:
+    list[list[int]]: A new matrix of the same size as `matrix`.
+
     matrice = [[row[x] **2 for x in range(len(matrix))] for row in matrix]
     return matrice
-#second method below
-"""
+    #second method below
+    """
 
 
 def square_matrix_simple(matrix=[]):
+    """
     transposed = []
     for row in matrix:
         transposed_row = []
@@ -15,3 +22,8 @@ def square_matrix_simple(matrix=[]):
             transposed_row.append(row[x] ** 2)
         transposed.append(transposed_row)
     return transposed
+    # third method
+    """
+
+    squared_rows = map(lambda row: list(map(lambda val: val**2, row)), matrix)
+    return list(squared_rows)
