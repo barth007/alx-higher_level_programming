ea8cad846924945afa0e3d0a7e5b9fc0da1b2fc6
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-16 14:59:49 +0000

    added task 2 file

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """
+    computes all unique integers in a list
+    
+    Args:
+    my_list(list): this is a lisst
+    
+    Return:
+    new_list[]
+    """
+    add_sum = 0
+    new_list = set(my_list)
+    for i in new_list:
+        add_sum = add_sum + i
+    return add_sum
