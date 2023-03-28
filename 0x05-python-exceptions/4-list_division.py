2b2bbdfc8ec96964737af1208a1f80d3527a30c4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-28 10:50:07 +0000

    done with task 5 and 6

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    function that divides element by element 2 list
+    @my_list_1 & @my_list_2: can contain any type (integer, string)
+    """
+    
