1b92f2f50d700de71660b5a064ebad049a22f334
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-28 09:28:11 +0000

    added file 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,4 +9,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
+        print("Inside result: {}".format(result))
         return ("{}".format(result))
