aa6af12f6e1b2aebd75a52ce00e5d31595eab138
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-28 07:33:54 +0000

    added the 2-safe_print_list_integers.py file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    safe_print_list_integers - function that prints the
+        first x elements of a list and only integers
+    @my_list: can contain any type integer, string.
+    @x: number of elements to access in my_list
+    """
+    counter = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                counter += 1
+        except ValueError:
+            pass
+    print(" ")
+    return (counter)
