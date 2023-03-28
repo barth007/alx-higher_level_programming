2a5b926dd4f9526ca087ad1c7d647b3e145a6a18
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-28 03:02:50 +0000

    added file 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     @value: can be any thing a string or integer
     """
     try:
-        print("{:d}\n".format(value))
+        print("{:d}".format(value))
         return (True)
     except ValueError:
         return (False)
