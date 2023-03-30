e5eb2947e18224da20ad12d73e953d116c6df344
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-30 08:39:43 +0000

    added the 5th task file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# 4-square.py
+"""Define a class Square. """
+
+
+class Square():
+    """Represents a square
+
+       Attributes:
+           attr1(int): this is size of the square
+
+    """
+    def __init__(self, size=0):
+        """initialize when created
+
+           Args:
+               size(int): a private field
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method
+
+           Args:
+               __size(int): the size to get
+           Return:
+               the set size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Setter method
+
+            Args:
+                value(int): the value to set to size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """method calculate the area of square
+
+           Args:
+               __size(int): the value
+
+           Return:
+               area of the square is returned
+           """
+        return (self.__size ** 2)
