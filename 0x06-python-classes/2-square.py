8e406b7308827d4066f6c5b46ad0e4c4af5e9227
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-30 01:11:47 +0000

    added the third task file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 2-square.py
+"""Define a Class Square"""
+
+
+class Square():
+    """Represents a class Square
+
+       Attributes:
+           attr1(int): This a private field of an integer data type
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new instance
+
+           Args:
+               size(int): A size of a square
+           """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
