16299bda094b186158f7b37ca87bb0607be7ce1b
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-05-17 02:44:32 +0000

    added task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,10 @@
+--  This script creates a table called firsttable in the current database in MySQL server.
+--  Attributes:
+-- id INT,
+-- name VARCHAR(256)
+-- if the table firsttable already exists, the script fails.
+
+CREATE TABLE IF NOT EXISTS first_name(
+    id int,
+    name varchar(256)
+    )
