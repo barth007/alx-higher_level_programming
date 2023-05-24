bdbec78f5118c148b7e80188fa52ceb5f8f784e7
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-05-24 03:33:44 +0000

    added task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,2 @@
 -- This script creates the MySQL server user_0d_1
-    CREATE USER 
-    IF NOT EXISTS 
-        'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
