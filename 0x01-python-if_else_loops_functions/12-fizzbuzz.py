9c0de16a86989b2b37f09b2d594388111eb9fcb8
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-10 17:57:39 +0000

    added task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 def fizzbuzz():
     """fizzbuzz prints the number from 1 to 100 seperated by a space.
     """
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
