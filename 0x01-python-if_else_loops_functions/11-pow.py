f5d2a918ef72d35ce945cb43236028af330e24e4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-10 12:25:08 +0000

    added task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """pow: computes a to the power of b and return the value
+
+      Args:
+          a(int):operand
+          b(int):operand
+     Return
+         a^b
+    """
+    return a ** b
