a3d23016e707310417a36cb27d613ee83921a64b
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-10 07:17:02 +0000

    added task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    """ print_last_digit: print the last digit of a number
+
+        Args:
+           number
+    """
+    if number < 0:
+        number = abs(number)
+    last_number = number % 10
+    print("{:d}".format(last_number), end="")
+    return last_number
