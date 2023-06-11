d3c6110c365062a82fdc702b550a3b03dbc99622
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-11 18:46:04 +0000

    added task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    return str[:n] + str[n + 1:]
+    if n >= 0:
+        new_str = str[:n] + str[n + 1:]
+        return new_str
+    else:
+        return str
