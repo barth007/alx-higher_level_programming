1ac08bdc82bff891428a43dc9d3a2cf6c1d615b2
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-14 04:00:56 +0000

    added task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (args[2]) {
+  let num = parseInt(args[2]);
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
