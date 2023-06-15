21672fa5fa9e592a656848d0842698288a969cb1
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-15 19:44:01 +0000

    added task 3 and README.md file

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ((w && h !== 0) && (w && h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
