7a4968a04bc558c50e7395f75cd7e4aec9146848
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-15 20:30:35 +0000

    added task 4 and README.md file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (c = 'X') {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    this.width = this.height;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
