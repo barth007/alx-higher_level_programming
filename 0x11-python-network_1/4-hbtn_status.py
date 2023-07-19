7727ec81b56454f9665d3806a317d0db860e9561
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-19 13:45:25 +0000

    added task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,9 @@
    This script fetches https://alx-intranet.hbtn.io/status
 """
 import requests
-response = requests.get('https://alx-intranet.hbtn.io/status')
-print("Body response:")
-print("\t -type: {}".format(type(response.text)))
-print("\t -content: {}".format(response.text))
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t -type: {}".format(type(response.text)))
+    print("\t -content: {}".format(response.text))
