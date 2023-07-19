8e2a119039d2f5993b8bb76a1a9ab9eb0586c951
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-19 15:12:58 +0000

    added task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,8 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    try:
-        response.raise_for_status
-        print(response.text)
-    except requests.exceptions.HTTPError as e:
+    if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
