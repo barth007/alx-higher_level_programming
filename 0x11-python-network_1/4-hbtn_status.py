c2e69480d39322c576aa632113564629092c30e6
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-19 14:04:37 +0000

    added task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,14 +5,10 @@
 import requests
 
 
-def fetch_resource(url):
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     response.raise_for_status()
     print("Body response:")
     print("\t -type: {}".format(type(response.text)))
     print("\t -content: {}".format(response.text))
-
-
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    fetch_resource(url)
