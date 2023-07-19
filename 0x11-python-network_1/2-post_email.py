d63dbb8e182340b74c80245915ac131380daf6eb
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-19 02:09:55 +0000

    added task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
+    values = {sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
