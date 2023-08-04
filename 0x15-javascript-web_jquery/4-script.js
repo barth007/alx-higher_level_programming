a49101cb1d507d082d50ead1e91b9107c0b5f3d2
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-08-04 23:33:57 +0000

    done with task 3

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$('#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else {
+    $('header').addClass('green');
+    $('header').removeClass('red');
+  }
+});
